86b29d7fb1643e5c9388f8567439b5178f01ecc3
Thiago Gimenes <59589489+thiagogimenes@users.noreply.github.com>
2020-11-30 23:48:28 -0300

    Refatorando código.
    Atribuído actions, refatoração de código.

--- parodify/features/step_definitions/login_steps.rb
+++ parodify/features/step_definitions/login_steps.rb
@@ -1,15 +1,12 @@
 Dado("que acesso a página de Login") do
-  visit "http://parodify.qaninja.com.br/"
-  click_on "Login"
+  goto_login
 end
 
 Quando("submeto minhas credenciais com: {string} e {string}") do |email, password|
-  find("#user_email").set email
-  find("#user_password").set password
-  click_on "Log in"
+  login_with(email, password)
 end
 
 Então("devo ver a mensagem de erro: {string}") do |expect_message|
-  message = find(".message .message-body")
-  expect(message.text).to eql expect_message
+  #message = find(".message .message-body") encapsulado a mensagem no arquivo actions
+  expect(login_alert).to eql expect_message
 end
--- parodify/features/support/env.rb
+++ parodify/features/support/env.rb
@@ -3,13 +3,16 @@ require "capybara"
 require "capybara/cucumber"
 
 require_relative "helpers"
+require_relative "actions"
 
 World(Helpers)
+World(Actions)
 
 Capybara.configure do |config|
   config.default_driver = :selenium
   config.default_max_wait_time = 10
   #por padrão o capybara busca um elemento css em 2 segundos. Pode ocorrer lentidão na internet e não dar tempo de buscar o elemento, neste caso usa-se consiguração para aumentar o tempo de busca do capybara, setando o max_time_wait de 10 segundos.
+  config.app_host = "http://parodify.qaninja.com.br"
 end
 
 AllureCucumber.configure do |config|
